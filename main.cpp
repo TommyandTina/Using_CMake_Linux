f3e29e20314ef73c862ba3fb6aeead9e3c207fb8
Long Trinh Tien <tommylong0209@gmail.com>
2024-06-03 15:03:49 +0700

    GITHUB ACTION

--- main.cpp
+++ main.cpp
@@ -10,26 +10,26 @@ int main(int argc, char* argv[]) {
 #endif
     cout<<"Hello, World!\n";
     cout<<argv[0]<<" VERSION :"<<MAIN_VERSION_MAJOR <<"."<<MAIN_VERSION_MINOR <<endl;
-    GLFWwindow *window;
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        exit( EXIT_FAILURE );
-    }
+    // GLFWwindow *window;
+    // if( !glfwInit() )
+    // {
+    //     fprintf( stderr, "Failed to initialize GLFW\n" );
+    //     exit( EXIT_FAILURE );
+    // }
 
-    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
-    if (!window)
-    {
-        fprintf( stderr, "Failed to open GLFW window\n" );
-        glfwTerminate();
-        exit( EXIT_FAILURE );
-    }
+    // window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    // if (!window)
+    // {
+    //     fprintf( stderr, "Failed to open GLFW window\n" );
+    //     glfwTerminate();
+    //     exit( EXIT_FAILURE );
+    // }
     
-    while( !glfwWindowShouldClose(window) )
-    {
-        // Swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+    // while( !glfwWindowShouldClose(window) )
+    // {
+    //     // Swap buffers
+    //     glfwSwapBuffers(window);
+    //     glfwPollEvents();
+    // }
     return 0;
 }
