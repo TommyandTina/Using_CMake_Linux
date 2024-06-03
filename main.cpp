2c036bf99ad33dad5e0f3c67ac05b6f6d033f498
Long Trinh Tien <tommylong0209@gmail.com>
2024-06-03 13:13:49 +0700

    add submodule and pull down

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <adder.h>
+#include <GLFW/glfw3.h>
+
 using namespace std;
 
 int main() {
     cout << adder(1, 2) << endl;
     cout<<"Hello, World!\n";
+
+    GLFWwindow *window;
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     return 0;
 }
